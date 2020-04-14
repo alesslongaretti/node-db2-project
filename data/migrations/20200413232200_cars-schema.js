14acd60ab3df2eea1ca8d6408cd76cd71126cfad
alesslongaretti <alesslongaretti@gmail.com>
2020-04-13 23:50:38 -0400

    designed and wrote a schema for the cars table, configured knex to connect to car-dealer.db3

---
+++ data/migrations/20200413232200_cars-schema.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('cars', tbl => {
+      tbl.increments();
+      tbl.text('VIN', 128).notNullable();
+      tbl.text('make', 128).notNullable();
+      tbl.text('model', 128).notNullable();
+      tbl.integer('mileage').notNullable();
+      tbl.text('transmission type', 128);
+      tbl.text('status of title', 128);
+
+  })
+};
+
+exports.down = function(knex) {
+   return knex.schema.dropTableIfExists('cars');
+};
